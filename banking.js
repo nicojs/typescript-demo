57b2c5b49d1d3c269b195bf8386a48d1d99e27f4
nicojs <jansennico@gmail.com>
2016-01-12 16:55:53 +0100

    Initial commit - added banking.js

---
+++ banking.js
@@ -0,0 +1,28 @@
+Object.defineProperty(window, "BankingApp", { value: "v1.0.0", readonly: true });
+
+function money() {
+    return parseInt(document.getElementById('price'));
+}
+
+/*
+ * @param {(string | string[])} ibanNumber - one or more iban account numbers to send the money to
+ */
+function sendTo(ibanNumber) {
+    function send(iban) {
+        console.log("Sending " + money + " to " + iban);
+
+        if (money > 100) {
+            // TODO
+            console.log('Sending lots of money, need additional verification')
+        }
+    }
+    
+    // If its an array, loop over it
+    if (ibanNumber.length) {
+        ibanNumber.forEach(function (iban) {
+            send(iban)
+        });
+    } else {
+        send(iban);
+    }
+}
